da9e97aae821e93c6e5b78f249a9025e82ff0f21
Marion Renard <mrenard37@gmail.com>
2020-11-09 15:36:04 +0100

    work on homepage

--- frontend/php/includes/header.php
+++ frontend/php/includes/header.php
@@ -1,23 +1,22 @@
 <header>
-    <div id="burger">
-        <div class="burger-line"></div>
-        <div class="burger-line"></div>
-        <div class="burger-line"></div>
-        <div id="burger-mask"></div>
-    </div>
-    <div id="nav-menu" class="nav-menu">
-        <div id="times-icons"></div>
-        <ul>
-            <a href=""><li>La commision centre</li></a>
-            <a href=""><li>Formations</li></a>
-            <a href=""><li>Concours et festivals</li></a>
-            <a href=""><li>Agenda</li></a>
-            <a href=""><li>Contact</li></a>
-        </ul>
+    <a href="http://localhost:8888/CRPV/frontend/php/"><img id="logo" src="../assets/img/logo.png"></a>
+    <div class="menu-container">
+        <img class="fas fa-bars"></img>
+        <nav>
+            <ul>
+                <a href=""><li>La commision centre</li></a>
+                <a href=""><li>Formations</li></a>
+                <a href=""><li>Concours et festivals</li></a>
+                <a href=""><li>Agenda</li></a>
+                <a href=""><li>Contact</li></a>
+            </ul>
+        </nav>
     </div>
 </header>
+
+
 <script type="text/javascript">
-    let menuButton      = document.getElementById('burger-mask');
+    /*let menuButton      = document.getElementById('burger-mask');
     let menuNav         = document.getElementById('nav-menu');
     let menuCloseButton = document.getElementById('times-icons');
     menuButton.addEventListener("click", () => {
@@ -26,7 +25,5 @@
 
     menuCloseButton.addEventListener("click", () => {
         menuNav.classList = 'nav-menu';
-    })
-
-    
+    })*/
 </script>
