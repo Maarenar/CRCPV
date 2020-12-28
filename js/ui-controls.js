b095789edeedcf701343c3c29a1cd699d33aa55d
Marion Renard <mrenard37@gmail.com>
2020-12-28 23:28:58 +0100

    implementation responsive

--- js/ui-controls.js
+++ js/ui-controls.js
@@ -1,8 +1,10 @@
 /**
- * Close hamburger menu
+ * Control mobile menu
  */
-
-function closeMenu(){
-    var i = document.getElementById("menu");
-    i.className = "menu_closed";
-}
+ window.addEventListener("load", () => {
+    document.getElementById('open-menu').addEventListener("click", () =>{
+        console.log('yo');
+        document.getElementById('nav-menu').style.display = 'block';
+        document.getElementById('nav-menu').classList.toggle('mobile-menu');
+    })
+ });
